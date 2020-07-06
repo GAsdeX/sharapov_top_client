0c3bf67954c1c9db38eaa9a74f381dd8c48b8198
hryhoriichaikovskyi <hryhorii.chaikovskiy@ext.conrad.com>
2020-07-06 23:22:42 +0300

    Fixed module

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
-export const BASE_URL = 'http://185.25.116.11:1337'
-export const SMTP_URL = 'http://185.25.116.11:3001'
+export const BASE_URL = 'https://sharapov.top:1337'
+export const SMTP_URL = 'https://185.25.116.11:3001'
 
 
 export const URL = {
