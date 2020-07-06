f8b7209a7e6aaf0fa16f05177423c72ca8612d3e
hryhoriichaikovskyi <hryhorii.chaikovskiy@ext.conrad.com>
2020-07-06 03:05:25 +0300

    Added about section

--- src/components/presentational/About/index.js
+++ src/components/presentational/About/index.js
@@ -1,20 +1,29 @@
-import React from "react"
+import React, { useState } from "react"
 import Masonry from 'react-masonry-css'
 import { Container, Row, Col } from "react-bootstrap";
 
 import {FullScrennView} from "../FullScreenView";
 import {AboutText} from "./AboutText";
 import {certs} from "../../../img/certs"
+
+import FsLightbox from 'fslightbox-react';
+
 import style from "./About.module.scss";
 
 export const About = () => {
-
+    const [toggler, setToggler] = useState(false);
+    const [slide, setSlide] = useState(false);
 
     return (
         <FullScrennView
             title="О Докторе"
             className={style.About}
         >
+            <FsLightbox
+                toggler={toggler}
+                sources={certs}
+                slide={slide}
+            />
             <Container>
                 <Row>
                     <Col
@@ -31,10 +40,14 @@ export const About = () => {
                             columnClassName="my-masonry-grid_column"
                         >
                             {
-                                certs.map((i) => (
-                                    <img src={i}>
-
-                                    </img>
+                                certs.map((image, index) => (
+                                    <img
+                                        src={image}
+                                        onClick={() => {
+                                            setToggler(!toggler)
+                                            setSlide(index + 1)
+                                        }}
+                                    />
                                 ))
                             }
                         </Masonry>
